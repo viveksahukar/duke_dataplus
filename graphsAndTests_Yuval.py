b2033fe1efe83e0c70fa977725fb8fe5e54b72d2
YuvalMedina <yuval.medina@duke.edu>
2019-06-03 11:49:33 -0400

    made some progress with my fourier transform/music things

--- graphsAndTests_Yuval.py
+++ graphsAndTests_Yuval.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as pltdates
 import datetime as dt
+import scipy as sy
+import scipy.fftpack as syfp
+import pylab as pyl
 
 myfile = pd.read_csv("flagged_sites.csv", sep=',', parse_dates=True)
 
@@ -21,11 +24,12 @@ def plotGraph(DataForm):
     #use on data extracted using 'getData' helper function
     dates = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S').date() for d in DataForm.dateTimeUTC]
     datenums = pltdates.date2num(dates)
-    plt.plot_date(dates, DataForm.value)
+    fig1, ax1 = plt.subplots();
+    ax1.plot_date(dates, DataForm.value)
     region = DataForm.loc[:, 'regionID']
     site = DataForm.loc[:,'siteID']
     variable = DataForm.loc[:,'variable']
-    plt.title(region.iloc[0] + ', ' + site.iloc[0] + ', ' + variable.iloc[0])
+    ax1.set_title(region.iloc[0] + ', ' + site.iloc[0] + ', ' + variable.iloc[0])
     return
 
 #unique vars:
@@ -37,7 +41,7 @@ def plotGraph(DataForm):
 # 'WaterTemp2_C']
 
 NC_Eno_WaterTempC = getData(myfile, 'NC', 'Eno', 'WaterTemp_C')
-plotGraph(NC_Eno_WaterTempC)
+#plotGraph(NC_Eno_WaterTempC)
 
 ranges = pd.DataFrame({'DO_mgL': [0, 25],
                         'WaterTemp_C': [-5, 50],
@@ -52,5 +56,20 @@ def basicRangeCleaner(DataForm):
     range = ranges.loc[:,variables.iloc[0]]
     return DataForm.loc[(DataForm['value']>range[0]) & (DataForm['value']<range[1])]
 
-NC_Eno_WaterTempC_basicClean = basicRangeCleaner(NC_Eno_WaterTempC)
-plotGraph(NC_Eno_WaterTempC_basicClean)
+NC_Eno_WaterTempC_basicRange = basicRangeCleaner(NC_Eno_WaterTempC)
+plotGraph(NC_Eno_WaterTempC_basicRange)
+
+#some fourier transform stuff I'm messing around with for music (?)
+NC_Eno_WaterTempC_fft = syfp.rfft(NC_Eno_WaterTempC_basicRange.loc[:,'value'])
+x = sy.linspace(0.0001, 0.0001*len(NC_Eno_WaterTempC_basicRange), num=len(NC_Eno_WaterTempC_basicRange))
+fig2, ax2 = plt.subplots();
+ax2.plot(x, NC_Eno_WaterTempC_fft)
+ax2.set_title("Eno, NC, fft deconstruction")
+NC_Eno_WaterTempC_fft_filtered = np.array(filter(lambda x: x <= 10, NC_Eno_WaterTempC_fft))
+
+NC_Eno_WaterTempC_fft_flat = syfp.ifft(NC_Eno_WaterTempC_fft_filtered)
+fig3, ax3 = plt.subplots();
+dates = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S').date() for d in NC_Eno_WaterTempC_basicRange.dateTimeUTC]
+datenums = pltdates.date2num(dates)
+ax3.plot_date(dates, NC_Eno_WaterTempC_fft_flat)
+ax3.set_title("Eno, NC, fft flattening")
