5f8b2844eced9441268b5175db20fd9b12a70fcb
YuvalMedina <yuval.medina@duke.edu>
2019-06-05 11:06:55 -0400

    some fixes to upstream (deleted empty file2, file3), some more updates to gapFill

--- gap_filler_Yuval.py
+++ gap_filler_Yuval.py
@@ -9,34 +9,28 @@ import pylab as pyl
 
 from main_functions import readFile, getData, plotGraph
 
-myfile = readFile();
-
-NC_Eno_WaterTempC = getData(myfile, 'NC', 'Eno', 'WaterTemp_C')
-plotGraph(NC_Eno_WaterTempC)
+myfile = readFile()
 
 #given a data form, and a start and end, which are dates in the same format as in the main .csv file
-def gapFill(DataForm, start, end):
-    start_date = dt.datetime.strptime(start,'%Y-%m-%d %H:%M:%S').date()
-    end_date = dt.datetime.strptime(end,'%Y-%m-%d %H:%M:%S').date()
+def gapFill(DataForm, start_date, end_date):
     duration = dt.timedelta(end_date) - dt.timedelta(start_date)
-    duration /= dt.timedelta(minutes=15)
     start_hour = start_date.hours
     start_minute = start_date.minutes
-    startindex = 0
-    for i in xrange(96):        #how many 15 minute intervals exist in one day
-        thisdate = dt.datetime.strptime(DataForm.iloc[i,0],'%Y-%m-%d %H:%M:%S').date()
-        if(thisdate.hours = start_hour & thisdate.minutes = start_minute):
-            startindex = i
-            break
-        #need to fix this loop (and all loops) because DataForm will have multiple entries for one date!
     i = 0
-    square_difs = np.array((len(DataForm)-startindex)/96, 1)
-    skipindex = ( dt.timedelta(start_date) - dt.timedelta(dt.datetime.strptime(DataForm.iloc[startindex,0],'%Y-%m-%d %H:%M:%S').date()) ) / dt.timedelta(minutes = 15)
-    while i < len(square_difs):
-        if(i = skipindex):
-            i += 96
-            continue
-        sum = 0
-        for j in xrange(96):
+    while(DateForm.iloc[i,2].hours != start_hour & DateForm.iloc[i,2].minutes != start_minute):        #how many 15 minute intervals exist in one day
+        i++
+    square_difs = dict()
+    while(DataForm.iloc[i,2] < DataForm['dateTimeUTC'].iloc(-1) - duration):
+        if(DateForm.iloc[i,2] == start_date):
+            while(DataForm.iloc[i,2] <= end_date):
+                i++
+        else:
+            dictindex = DataForm.iloc[i,2]
+            while(DataForm.iloc[i,2] < dictindex + duration):
+                currentdate = DataForm.iloc[i,2]
+                while(DataForm.iloc[i,2] == currentdate):
 
-        i += 96
+            sum = 0
+            for j in xrange(96):
+
+            i += 96
